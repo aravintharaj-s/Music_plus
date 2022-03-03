03cccbcf7987ef30c1ddff80c44486165dc4f84c
manoj ravichandran <manojravichandran12@gmail.com>
2022-03-04 03:54:09 +0530

    ui changes

--- client/src/pages/Page404.js
+++ client/src/pages/Page404.js
@@ -9,9 +9,9 @@ function Page404() {
     <>
         <ParticleBackground/>
         <div style={{maxWidth:'1400px',margin:'100px 100px'}}>
-          <div class="container-fluid" >
-              <div class="row" style={{display:'flex',justifyContent:'center',alignItems:'center',margin:'auto'}}>
-                  <div class="col col-md-4 col-sm-12" style={{marginRight:'50px'}}>
+          <div class="container-fluid" style={{display:'flex',justifyContent:'center',alignItems:'center'}}>
+              <div class="row" style={{display:'flex',justifyContent:'center',alignItems:'center',}}>
+                  <div class="col col-md-4 col-sm-12" style={{marginRight:'70px'}}>
                     <Player
                         autoplay
                         loop
@@ -20,9 +20,9 @@ function Page404() {
                         >
                     </Player>
                   </div>
-                  <div class="col col-md-6 col-sm-12" style={{textAlign:'center',background:'red'}} >
-                      <p style={{fontSize:'45px',width:'100%'}} >Page Not Found</p>
-                      <p>Check if there is a typo</p>
+                  <div class="col col-md-6 col-sm-12" style={{textAlign:'center',color:'white'}} >
+                      <p style={{fontSize:'37px',width:'100%',color:'white'}} >Page Not Found</p>
+                      <p style={{color:'white'}}>Check if there is a typo in <span style={{color:'violet'}}>{window.location.href}</span></p>
                   </div>
               </div>
           </div>
