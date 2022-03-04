1409e4331969e62e916ec646a00dfcabbe98f5ca
Aravintharaj <aravioct@gmail.com>
2022-03-04 15:15:01 +0530

    drop added

--- client/src/components/profile.js
+++ client/src/components/profile.js
@@ -1,14 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './profile.css'
 import {CgUser,CgChevronDown} from 'react-icons/cg';
 
 function Profile() {
+    const [isActive,setIsActive]=useState(false);
   return (
     <div className="profile">
-        <div className="drop">
+        <div className="drop" onClick={(e)=>setIsActive(!isActive)}>
             <CgUser style={{fontSize:'20px',color:'white',margin:"5px"}}/>
             User
             <CgChevronDown style={{fontSize:'20px',color:'white',margin:"5px"}}/>
+            {isActive&&(<div className="content">
+                <div className="item">Account</div>
+                <hr style={{backgroundColor: '#00ffff',margin:'0px',padding:'0px',height:'2px'}}/>
+                <div className="item">Profile</div>
+                <hr style={{backgroundColor: '#00ffff',margin:'0px',padding:'0px',height:'2px'}}/>
+                <div className="item">Logout</div>
+            </div>)}
         </div>
     </div>
   )
