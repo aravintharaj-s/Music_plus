ac88d33c11f4aa327fca3e07c013d06cfc4592c4
Aravintharaj <aravioct@gmail.com>
2022-03-12 14:55:10 +0530

    error fixed

--- server/app.js
+++ server/app.js
@@ -7,17 +7,17 @@ dotenv.config({ path: './config.env' });
 require('./db/conn');
 
 app.use(express.json());
-
+app.use(require('./router/auth'));
 const PORT=process.env.PORT;
-/*const User=require('./model/userSchema')*/
+/*const User=require('./model/userSchema')
 
-// app.use(require('./router/auth'));
+
 
 app.get('/',(req,res)=>{
 
     res.send('helllo');
     console.log(req.url);
-});
+});*/
 
 /*app.get('/about',midddleware,(req,res)=>{
     res.send('Hello from about server app js');
@@ -25,7 +25,7 @@ app.get('/',(req,res)=>{
 
 app.get('/signin',(req,res)=>{
     res.send('Hello from sigin page server');
-});*/
+});
 
 app.get('/signup',(req,res)=>{
     res.send('Hello from signup page server');
@@ -35,7 +35,7 @@ app.use((req,res)=>
 {
    res.status(404).send("404 Error");
 
-});
+});*/
 
 
 app.listen(PORT,()=>{
